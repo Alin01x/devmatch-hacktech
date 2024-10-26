6ecfba7b2834391ffafd32a511c5e8cb9b0ec6a6
Alin Hucea <alinhucea@protonmail.com>
2024-10-26 18:14:23 +0300

    Remove Level Experience from both CVs and Job Descriptions

--- src/app/api/_extractors/extractCvData.ts
+++ src/app/api/_extractors/extractCvData.ts
@@ -1,17 +1,17 @@
 import { sendGPTRequest } from "../_utils/openAI";
-import { ExperienceLevel } from "@/types/Enums";
 import { EXTRACT_CV_DATA_PROMPT } from "../_utils/prompts";
 
 export const extractCvData = async (
   content: string
 ): Promise<{
+  name: string;
   industries: string[];
   skills: string[];
-  experienceLevel: ExperienceLevel;
 }> => {
   try {
     const gptResponse: {
-      experienceLevel: ExperienceLevel;
+      name: string;
+
       skills: string[];
       industries: string[];
     } = await sendGPTRequest({
@@ -20,17 +20,16 @@ export const extractCvData = async (
     });
 
     // TODO:  handle case for which GPT couldn't extract all the data
-    //        for which we can ask the user to manually set the fields in the UI
 
     return {
-      experienceLevel: gptResponse.experienceLevel ?? "junior" as ExperienceLevel,
+      name: gptResponse.name ?? "",
       skills: gptResponse.skills ?? [],
       industries: gptResponse.industries ?? [],
     };
   } catch (error) {
     console.error("Error extracting experience level:", error);
     return {
-      experienceLevel: "junior" as ExperienceLevel,
+      name: "",
       skills: [],
       industries: [],
     };
--- src/app/api/_utils/prompts.ts
+++ src/app/api/_utils/prompts.ts
@@ -1,24 +1,16 @@
-export const EXTRACT_JOB_EXPERIENCE_LEVEL_PROMPT = `
-    You are an AI assistant that analyzes job descriptions and determines the experience level required.
-    Output must be JSON in the following format:
-    {
-        "experienceLevel": "junior" | "mid" | "senior"
-    }
-`;
-
 export const EXTRACT_CV_DATA_PROMPT = `
     You are an AI assistant that extracts data from a CV.
     Output must be JSON in the following format:
     {
-        "experienceLevel": "junior" | "mid" | "senior",
+        "name": "Person's name",
         "skills": ["skill1", "skill2", "skill3"],
         "industries": ["industry1", "industry2", "industry3"]
     }
 
     Example:
     {
-        "experienceLevel": "mid",
-        "skills": ["React", "Python", "SQL", "Machine Learning", "Data Visualization"],
+        "name": "John Doe",
+        "skills": ["React", "Node", "Python", "SQL", "Docker", "Figma],
         "industries": ["Finance", "Healthcare"]
     }
 `;
