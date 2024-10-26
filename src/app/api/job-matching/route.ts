179c8d4b2abf15bb02e121bf29b126324e34d522
Alin Hucea <alinhucea@protonmail.com>
2024-10-26 19:37:18 +0300

    Finalyze Semantic Analysis for Industry matching

--- src/app/api/job-matching/route.ts
+++ src/app/api/job-matching/route.ts
@@ -1,79 +1,20 @@
 import { headers } from "../_utils/requestHeaders";
-import { JobDescription } from "../../../types/JobDescription";
+import { JobDescription, Skills } from "../../../types/JobDescription";
 import { supabase } from "@/lib/supabase";
-import { findTopMatches } from "../_utils/semanticAnalysis";
 import { SKILLS } from "@/constants/skills";
+import { CV } from "@/types/CV";
+import { industryAnalysisService } from "../_services/IndustryAnalysisService";
+import { MatchingCV } from "@/types/MatchResult";
+import { log } from "console";
 
 export async function POST(request: Request) {
   try {
     const { job_title, industry, detailed_description, skills } =
       (await request.json()) as Omit<JobDescription, "id" | "createdAt">;
 
-    for (const key in skills) {
-      const weight = skills[key];
-      delete skills[key];
-      skills[getSkillWithProperCase(key)] = weight;
-    }
-
     // TODO: implement data validation
 
-    // Store Job Descriptions in the database
-    const jobDescriptionData = {
-      detailed_description: detailed_description,
-      job_title: job_title,
-      industry,
-      skills,
-    };
-
-    const { data, error } = await supabase
-      .from("job_descriptions")
-      .insert(jobDescriptionData)
-      .select()
-      .single();
-
-    if (error) {
-      console.error("Error saving job description:", error);
-      return new Response(
-        JSON.stringify({ error: "Failed to save Job Description." }),
-        {
-          status: 500,
-          headers: { ...headers, "Content-Type": "application/json" },
-        }
-      );
-    }
-
-    // Find Matching CV
-
-    // Read all CVs from the database with at least 1 skill match
-    const { data: cvs, error: cvError } = await supabase
-      .from("cvs")
-      .select("*")
-      .overlaps("skills", Object.keys(skills));
-
-    if (cvError) {
-      console.error("Error fetching CVs:", cvError);
-      return new Response(
-        JSON.stringify({ error: "Failed to fetch matching CVs." }),
-        {
-          status: 500,
-          headers: { ...headers, "Content-Type": "application/json" },
-        }
-      );
-    }
-
-    console.log("CVs with at least one skill", cvs.length);
-
-    // Semantic Analysis
-    const topMatches = findTopMatches(data, cvs);
-
-    console.log("Top Matches:", topMatches);
-    // Find all CVs having at least 50% match with the job description
-    // const matchingCVs = await findMatchingCVs(jobDescriptionData);
-
-    return new Response(JSON.stringify({ success: true, data }), {
-      status: 200,
-      headers: { ...headers, "Content-Type": "application/json" },
-    });
+    return handleJobMatching(job_title, industry, detailed_description, skills);
   } catch (e) {
     console.error("Error processing job description:", e);
     return new Response(
@@ -94,4 +35,68 @@ const getSkillWithProperCase = (skill: string) => {
     (s) => s.toLowerCase() === skill.toLowerCase()
   );
   return skillWithProperCase || skill;
+};
+
+export const handleJobMatching = async (
+  job_title: string,
+  industry: string,
+  detailed_description: string,
+  skills: Skills
+) => {
+  // Fix letter casing for skills
+  for (const key in skills) {
+    const weight = skills[key];
+    delete skills[key];
+    skills[getSkillWithProperCase(key)] = weight;
+  }
+
+  // Store Job Descriptions in the database
+  const jobDescriptionData = {
+    detailed_description: detailed_description,
+    job_title: job_title,
+    industry,
+    skills,
+  };
+
+  const { data, error: insertError } = await supabase
+    .from("job_descriptions")
+    .insert(jobDescriptionData)
+    .select()
+    .single();
+  if (insertError) throw insertError;
+
+  // MATCHING ALGORITHM
+
+  // Step 1: Read all CVs with at least 1 skill match
+  const { data: potentialCVs, error: readError } = await supabase
+    .from("cvs")
+    .select("*")
+    .overlaps("skills", Object.keys(skills));
+  if (readError) throw readError;
+
+  console.log("Step 1: Potential", potentialCVs.length);
+
+  // Step 2: Industry Knowledge Criteria (10%)
+  // Perform semantic analysis on each CV
+
+  const matchingCVs: MatchingCV[] = [];
+
+  for (const cv of potentialCVs) {
+    const { score, reasoning } =
+      industryAnalysisService.analyzeIndustryRelevance(cv, industry);
+
+    matchingCVs.push({
+      cv,
+      industryScore: score,
+      industryReasoning: reasoning,
+    });
+  }
+
+
+  console.log("Step 2: Matching CVs", matchingCVs);
+
+  return new Response(JSON.stringify({ success: true, data }), {
+    status: 200,
+    headers: { ...headers, "Content-Type": "application/json" },
+  });
 };
